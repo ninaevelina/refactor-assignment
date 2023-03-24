e23c0eacf1efeecc6790cf82851dac669899b726
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-03-24 11:37:25 +0100

    (refactor): add try/catch to podcastService - closes #19

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ VG
 - [ ] Gör en tillgänglighetsgranskning av sidan
 - [ ] Utnyttja features i Sass i CSS:en
 - [ ] Rensa bort paket som inte används
-- [ ] Hantera fel i API-anropet
+- [ x ] Hantera fel i API-anropet
 - [ ] Gör en Lighthouse-analys
 - [ ] Läs av utvecklingsmiljön
 - [ ] Enhetlig syntax i CSS:en
--- src/ts/services/podcastService.ts
+++ src/ts/services/podcastService.ts
@@ -3,6 +3,11 @@ import type { IPodcast } from '../models/IPodcast'
 import type { ISRResponse } from '../models/ISRResponse'
 
 export async function getSRPodcasts (): Promise<IPodcast[]> {
-  const response = await axios.get<ISRResponse>('https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false')
-  return response.data.programs
+  try {
+    const response = await axios.get<ISRResponse>('https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false')
+    return response.data.programs
+  } catch (error) {
+    console.log(error)
+    console.log('Something went wrong! Cannot get podcasts, please try again later! :)')
+  }
 }
