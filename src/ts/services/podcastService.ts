a39f1c452f5ac4110510479b34d07aead2671217
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-03-24 14:16:43 +0100

    (refactor): move api-url to .env - closes Remove API-url from source code #20

--- src/ts/services/podcastService.ts
+++ src/ts/services/podcastService.ts
@@ -2,12 +2,15 @@ import axios from 'axios'
 import type { IPodcast } from '../models/IPodcast'
 import type { ISRResponse } from '../models/ISRResponse'
 
+const SRApiUrl: string = import.meta.env.VITE_APP_API_URL as string
+
 export async function getSRPodcasts (): Promise<IPodcast[]> {
   try {
-    const response = await axios.get<ISRResponse>('https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false')
+    const response = await axios.get<ISRResponse>(SRApiUrl)
     return response.data.programs
   } catch (error) {
     console.log(error)
     console.log('Something went wrong! Cannot get podcasts, please try again later! :)')
+    return []
   }
 }
